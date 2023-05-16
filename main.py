fa20bdddaac00f54cb5034e363804a90e7235e09
BezawitEtsubneh <121513006+BezawitEtsubneh@users.noreply.github.com>
2023-05-16 12:16:00 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
-#this part is the speach recoginzer part of the alexa project that ai have begun
 import speech_recognition as sr
-
+import pyttsx3
 
 listener =sr.Recognizer()
-try:
-    with sr.Microphone() as source:
-        print('listen...')
+engine=pyttsx3.init()
+voices=engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
+
+
+def talk(text):
+
+ engine.say('text')
+ engine.runAndWait()
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
         voice = listener.listen(source)
         command =listener.recognize_google(voice)
-        print(command)
+        command=command.lower()
+        if 'alexa' in command:
+            command=command.replace('alexa','')
+            print(command)
+
 except:
+print('invalid message')
     pass
+                return command
+
+        def run_alexa():
+            command=take_command()
+            print command
+            if 'play' in command :
+                talk('playing')
+                print('playing')
+
+            run_alexa()
